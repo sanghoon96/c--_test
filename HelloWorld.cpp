22811979402e6b7d666b9a8db05540a555f50eb0
sanghoon96 <sanghoon96@naver.com>
2020-11-11 13:14:11 +0900

    commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>                                         // contain information that is necessary
+/*                                                          // Long Comment
+This is first comment
+*/
+using namespace std;                                        // What use to compiler
+int main()                                                  // where program execution begins
+{                                                           // Blocks
+    cout << "Hello World! \n";                                 // Semicolons
+    return 0;                                               // terminates main()function
+}
+
+/*
+~$ c++ ./HelloWorld.cpp                                     // compile and linking
+~$ ./a.out                                                  //execute
+Hello World!
+
+해보기
+"Hello World! \n"; -> "Hello World!";
+줄바꿈없이 실행된다.
+*/
