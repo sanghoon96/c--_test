2c13105903d191183dd66fd1f4f4206dafbcec8b
sanghoon96 <sanghoon96@naver.com>
2020-11-12 11:46:08 +0900

    commit

--- decisionifStatement.cpp
+++ decisionifStatement.cpp
@@ -2,21 +2,20 @@
 
 using namespace std;
 
-int function();
-
-int main()
+int function();                                                                             // 함수 선언
+int main()                                                                                  // 함수 호출
 {
-    int i = function();
-    cout << "Exact value of first is : " << i << endl;
-    return 0;
+    int i = function();                                                                     // i를 function이라고 정의
+    cout << "Exact value of first is : " << i << endl;                                      // print
+    return 0;                                                                               // 함수 종료
 }
 
-int function(){
+int function(){                                                                             // function의 정의
     int first = 100;
 
     if (first == 10)
     {
-        cout << "Value of first is 10" << endl;
+        cout << "Value of first is 10" << endl;                                             // first가 10이면 "Value of first is 10" 이라고 나옴
     }
     else if (first == 30)
     {
@@ -24,7 +23,7 @@ int function(){
     }
     else
     {
-        cout << "Value of first is not matching" << endl;
+        cout << "Value of first is not matching" << endl;                                   // first가 "Value of first is not matching"이면
     }
-    return first;
+    return first;                                                                           // first 값이 나온다
 }
