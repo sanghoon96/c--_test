f06ab622abbc11527525681df3d7c45a90fd8b03
sanghoon96 <sanghoon96@naver.com>
2020-11-12 10:15:59 +0900

    commit

---
+++ Function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int funcInside();                                       // function declaration
+int main(){
+    int i = funcInside();                               // function call
+    cout << "return value : " << i << endl;
+    return 0;
+}
+int funcInside(){                                       // function definition
+    cout << "int funcInside()" << endl;
+    return 8;
+}
