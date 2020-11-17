e9ec4204e449d0e6c9a04b7baf8cce68f7b75b6c
sanghoon96 <sanghoon96@naver.com>
2020-11-17 13:16:00 +0900

    Multi-dimensional Arrays

---
+++ MultiDimensionalArrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int row, column;
+    char char_row, char_column;
+    cout << "Enter Row and Column: ";
+    cin >> row >> column;
+    char **ptr_matrix = new char *[row]; // Dynamic Allocation
+    for (int i = 0; i < row; i++)
+        ptr_matrix[i] = new char[column];
+    cout << "Matrix Size : " << row * column << endl;
+    for (int i = 0; i < row; i++) // output each array element's value
+        for (int j = 0; j < column; j++)
+        {
+            ptr_matrix[i][j] = char_row;
+        }
+    for (int i = 0; i < row; i++) // output each array element's value
+        for (int j = 0; j < column; j++)
+        {
+            cout << "ptr_matrix[" << i << "][" << j << "]: " << ptr_matrix[i][j] << endl;
+        }
+    delete[] ptr_matrix;
+    return 0;
+}
